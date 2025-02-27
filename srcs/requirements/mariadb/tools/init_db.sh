b57165e5ec223a5df966c56f03e250d5ae02edb0
Michelle <m.stegeman@me.com>
2025-02-27 13:20:48 +0100

    refactor MariaDB configuration and initialization scripts. now working

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,27 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
 # Start MariaDB
-echo "Starting MariaDB..."
 service mariadb start
 
 sleep 5
 
 # Setup MariaDB
-echo "Running additional database setup..."
+echo "Running database setup..."
 mariadb -u root << EOF
-USE my_database;
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_ADMIN_USER'@'%' IDENTIFIED BY '$DB_ADMIN_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_ADMIN_USER';
+FLUSH PRIVILEGES;
+USE $DB_NAME;
 CREATE TABLE IF NOT EXISTS users (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	username VARCHAR(50) NOT NULL,
 	password VARCHAR(12) NOT NULL,
-	email VARCHAR(100) UNIQUE NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL
 );
-ON DUPLICATE KEY UPDATE
-	username = VALUES(username),
-	password = VALUES(password),
-	email = VALUES(email);
 EOF
 
-mariadb -u root shutdown
+# Stop MariaDB
+service mariadb stop
 
+# Restart MariaDB in the foreground
+echo "Restarting MariaDB in the foreground..."
+exec mysqld_safe
 echo "Database setup complete."
