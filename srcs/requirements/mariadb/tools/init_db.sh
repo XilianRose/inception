401a974633f98cebff34da14d458ff9657aba31b
Michelle <m.stegeman@me.com>
2025-02-27 10:42:42 +0100

    add Docker Compose setup for MariaDB and Nginx + add environment file + add MariaDB dockerfile & depenencies (needs to be reviewed)

---
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Start MariaDB
+echo "Starting MariaDB..."
+service mariadb start
+
+sleep 5
+
+# Setup MariaDB
+echo "Running additional database setup..."
+mariadb -u root << EOF
+USE my_database;
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(50) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+);
+ON DUPLICATE KEY UPDATE
+	username = VALUES(username),
+	password = VALUES(password),
+	email = VALUES(email);
+EOF
+
+mariadb -u root shutdown
+
+
+
+
+echo "Database setup complete."
+
