ce4066fbb6a683e897e67195b2a380a3b0f7bdb4
mstegema <mstegema@student.codam.nl>
2025-02-27 15:54:38 +0100

    create init script for WordPress installation and configuration

---
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -0,0 +1,28 @@
+#!bin/sh
+
+# Download WordPress via CLI
+exho "Downloading WordPress CLI..."
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+chown -R www-data:www-data /var/www/wordpress
+
+echo "Downloading WordPress core files..."
+wp core download --path=/var/www/wordpress --allow-root
+
+# Configure WordPress
+echo "Configuring WordPress..."
+wp confic create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_ADMIN_USER --dbpass=$DB_ADMIN_PASSWORD --allow-root
+
+echo "Installing WordPress..."
+wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+wp user create $WP_USER_1 $WP_EMAIL_1 --role=$WP_ROLE_1 --user_pass=$WP_PASSWORD_1 --allow-root
+echo "WordPress setup complete."
+
+# Start PHP
+echo "Starting PHP setup..."
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
+mkdir /run/php
+exec php-fpm8.4 -F
+echo "PHP setup complete."
