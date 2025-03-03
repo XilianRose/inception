37815047a6ddd40313c1a2435b0214ffc05b768f
Michelle <m.stegeman@me.com>
2025-03-03 14:48:59 +0100

    update init_wp.sh to change WordPress download path, improve wp-config.php creation logic and skip sending of email to admin

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -6,17 +6,27 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-chown -R www-data:www-data /var/www/wordpress
-
 echo "Downloading WordPress core files..."
-wp core download --path=/var/www/wordpress --allow-root
+wp core download --path=/var/www/html/wordpress --allow-root
+
+chmod -R 755 /var/www/html/wordpress
+chown -R www-data:www-data /var/www/html/wordpress
 
 # Configure WordPress
 echo "Configuring WordPress..."
-wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_ADMIN_USER --dbpass=$DB_ADMIN_PASSWORD --allow-root
+cd /var/www/html/wordpress
+if [ ! -f /wp-config.php ]; then
+	wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_ADMIN_USER --dbpass=$DB_ADMIN_PASSWORD --allow-root
+	if [ $? -ne 0 ]; then
+		echo "Error creating wp-config.php"
+		exit 1
+	fi
+else
+	echo "wp-config.php already exists."
+fi
 
 echo "Installing WordPress..."
-wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USER_1 $WP_EMAIL_1 --role=$WP_ROLE_1 --user_pass=$WP_PASSWORD_1 --allow-root
 echo "WordPress setup complete."
 
