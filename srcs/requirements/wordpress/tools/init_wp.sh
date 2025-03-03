d73547cd36c3a0fccf6296860abdf82eed46f1f0
Michelle <m.stegeman@me.com>
2025-03-03 16:13:40 +0100

    update environment configuration for WordPress to use HTTPS and adjust Nginx root directory; refine WordPress installation script

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -8,7 +8,6 @@ mv wp-cli.phar /usr/local/bin/wp
 
 echo "Downloading WordPress core files..."
 wp core download --path=/var/www/html/wordpress --allow-root
-
 chmod -R 755 /var/www/html/wordpress
 chown -R www-data:www-data /var/www/html/wordpress
 
@@ -25,16 +24,17 @@ else
 	echo "wp-config.php already exists."
 fi
 
+# Install WordPress
 echo "Installing WordPress..."
-wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp core install --url=$WP_URL --title="$WP_TITLE" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USER_1 $WP_EMAIL_1 --role=$WP_ROLE_1 --user_pass=$WP_PASSWORD_1 --allow-root
 echo "WordPress setup complete."
 
 # Start PHP
 echo "Starting PHP setup..."
-sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/' /etc/php/7.4/fpm/pool.d/www.conf
 if [ ! -d "/run/php" ]; then
 	mkdir /run/php
 fi
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/' /etc/php/7.4/fpm/pool.d/www.conf
 echo "PHP setup complete."
 exec /usr/sbin/php-fpm7.4 -F
