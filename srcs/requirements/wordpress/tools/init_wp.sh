cc0080f693c986fb842c502542a2eaa8e19208c6
Michelle <m.stegeman@me.com>
2025-03-04 17:25:42 +0100

    update init_wp.sh to check for existing installations before downloading and configuring WordPress

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -2,33 +2,41 @@
 
 # Download WordPress via CLI
 echo "Downloading WordPress CLI..."
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+if [ -f /usr/local/bin/wp/wp-cli.phar ]; then
+	echo "WordPress CLI already installed."
+else
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
+fi
 
 echo "Downloading WordPress core files..."
-wp core download --path=/var/www/html/wordpress --allow-root
-chmod -R 755 /var/www/html/wordpress
-chown -R www-data:www-data /var/www/html/wordpress
+if [ -d /var/www/html/wordpress ]; then
+	echo "WordPress already downloaded."
+else
+	wp core download --path=/var/www/html/wordpress --allow-root
+	chmod -R 755 /var/www/html/wordpress
+	chown -R www-data:www-data /var/www/html/wordpress
+fi
 
 # Configure WordPress
 echo "Configuring WordPress..."
 cd /var/www/html/wordpress
-if [ ! -f /wp-config.php ]; then
-	wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_ADMIN_USER --dbpass=$DB_ADMIN_PASSWORD --allow-root
-	if [ $? -ne 0 ]; then
-		echo "Error creating wp-config.php"
-		exit 1
-	fi
-else
+if [ -f /wp-config.php ]; then
 	echo "wp-config.php already exists."
+else
+	wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_ADMIN_USER --dbpass=$DB_ADMIN_PASSWORD --allow-root
 fi
 
 # Install WordPress
 echo "Installing WordPress..."
-wp core install --url=$WP_URL --title="$WP_TITLE" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-wp user create $WP_USER_1 $WP_EMAIL_1 --role=$WP_ROLE_1 --user_pass=$WP_PASSWORD_1 --allow-root
-echo "WordPress setup complete."
+if [ -f /index.php ]; then
+	echo "WordPress already installed."
+else
+	wp core install --url=$WP_URL --title="$WP_TITLE" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+	wp user create $WP_USER_1 $WP_EMAIL_1 --role=$WP_ROLE_1 --user_pass=$WP_PASSWORD_1 --allow-root
+	echo "WordPress setup complete."
+fi
 
 # Start PHP
 echo "Starting PHP setup..."
